3c54ff02b59952119cea6428f71b0f2356e2b5e8
Ernesto <gpchuky@gmail.com>
2023-02-09 21:07:32 -0300

    se probaron cosas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
-
+const path = require('path')
 const PORT = 3000
 
+
+app.use(express.static(path.join(__dirname,"public")))
 app.listen(PORT, () => console.log('listening on port', PORT))
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.get('/producto', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views/producto.html'))
 })
